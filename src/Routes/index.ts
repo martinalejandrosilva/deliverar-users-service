90ff609f14deb0f61ba50394c5035452920d1d82
Martin Silva <martinalejandrosilva@gmail.com>
2023-10-10 18:25:02 -0300

    Add Field Validation

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -2,7 +2,10 @@ import express from "express";
 import AliveController from "../Controllers/alive";
 import UserController from "../Controllers/user";
 import AuthController from "../Controllers/auth";
+import { validationResult } from "express-validator";
 const router = express.Router();
+const { validateRegister, validateLogin } = require('../Validation/userValidation');
+
 
 router.get("/api/alive", (_req, res) => {
   const controller = new AliveController();
@@ -11,22 +14,26 @@ router.get("/api/alive", (_req, res) => {
   });
 });
 
-router.post("/api/user", (_req, res) => {
+router.post("/api/user",validateRegister ,(_req, res) => {
+  const errors = validationResult(_req);
+  if(!errors.isEmpty()){
+    return res.status(404).json({errors: errors.array()});
+  }
+
   const controller = new UserController();
+  
   const { name, email, password } = _req.body;
   controller.register({name,email,password}).then(response => {
     res.status(response.code).send(response.payload);
   });
-}
-);
+});
 
-router.post("/api/auth/login", (_req, res) => {
+router.post("/api/auth/login", validateLogin, (_req, res) => {
   const controller = new AuthController();
   const { email, password } = _req.body;
   controller.login({email,password}).then(response => {
     res.status(response.code).send(response);
   });
-}
-);
+});
 
 export default router;
